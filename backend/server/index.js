76144575f9c7c32680025e62612294e42ed37635
amalia-ullman <mali.ullman@gmail.com>
2022-08-07 12:28:13 -0700

    they talk sometimes

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.get("/test", (req, res) => {
+    res.json({message:"what up"})
+})
 app.listen(PORT, () => {
     console.log(`server listening on...${PORT}`);
 });
